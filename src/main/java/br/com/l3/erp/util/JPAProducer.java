06a598b436d59f1fe2faa5d45f4ab913a03e966e
Lucas <lucasjsm2001@gmail.com>
2025-12-16 02:43:29 -0300

    fix: refactor JPAProducer

--- src/main/java/br/com/l3/erp/util/JPAProducer.java
+++ src/main/java/br/com/l3/erp/util/JPAProducer.java
@@ -1,8 +1,5 @@
 package br.com.l3.erp.util;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
@@ -14,45 +11,18 @@ import javax.persistence.Persistence;
 @ApplicationScoped
 public class JPAProducer {
 
-    private static EntityManagerFactory factory;
+    // A fábrica é criada uma única vez para toda a aplicação.
+    private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("erpPU");
 
-    static {
-        // Cria um mapa para passar as configurações dinâmicas ao Hibernate
-        Map<String, String> properties = new HashMap<>();
-
-        // Lê as propriedades que foram definidas pelo AppInitializer (seja local ou no Railway)
-        // O AppInitializer roda antes deste bloco estático ser chamado (ao fazer login)
-        String driver = System.getProperty("db.driver");
-        String url = System.getProperty("db.url");
-        String user = System.getProperty("db.username");
-        String password = System.getProperty("db.password");
-
-        // Se as propriedades existirem, adiciona ao mapa para sobrescrever o persistence.xml
-        if (url != null) {
-            properties.put("javax.persistence.jdbc.driver", driver);
-            properties.put("javax.persistence.jdbc.url", url);
-            properties.put("javax.persistence.jdbc.user", user);
-            properties.put("javax.persistence.jdbc.password", password);
-        }
-
-        try {
-            // Cria a factory passando o mapa de propriedades
-            // Isso substitui os placeholders ${...} pelos valores reais
-            factory = Persistence.createEntityManagerFactory("erpPU", properties);
-        } catch (Throwable e) {
-            System.err.println("ERRO CRÍTICO AO INICIAR JPA: " + e.getMessage());
-            e.printStackTrace();
-            // Lança exceção para ver no log caso falhe
-            throw new ExceptionInInitializerError(e);
-        }
-    }
-
+    // Este método "produz" um EntityManager para cada requisição.
+    // O CDI o chamará automaticamente sempre que encontrar um @Inject EntityManager.
     @Produces
     @RequestScoped
     public EntityManager createEntityManager() {
         return factory.createEntityManager();
     }
 
+    // Este método fecha o EntityManager no final da requisição para liberar recursos.
     public void closeEntityManager(@Disposes EntityManager entityManager) {
         if (entityManager.isOpen()) {
             entityManager.close();
